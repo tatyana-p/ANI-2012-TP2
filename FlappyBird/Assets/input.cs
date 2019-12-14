0eda33f1493b2fe89a55fc6eec1f2b73e31c42e3
mathdubreuil <mathieu.dubreuil.1@ulaval.ca>
2019-12-14 18:24:21 -0500

    ajout du projet unity

---
+++ FlappyBird/Assets/input.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour
+{
+        private Animator anim;    
+    // Start is called before the first frame update
+    void Start ()
+    {
+      anim = GetComponent<Animator> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey("space"))
+      {
+        anim.GetComponent<Animator>().enabled = false;
+        anim.GetComponent<Animator>().enabled = true;
+        anim.Play("up");
+        anim.SetBool("updown", true);
+      }else if (Input.GetKeyUp(KeyCode.Space)){
+        anim.GetComponent<Animator>().enabled = false;
+        anim.GetComponent<Animator>().enabled = true;
+        anim.Play("down");
+        anim.SetBool("updown", true);
+      }else{
+        anim.SetBool("updown", false);
+      }
+    }
+}
---
+++ FlappyBird/Assets/transform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class transform : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public bool reset = false;
+    void Update ()
+    {
+		  transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+      
+      if (Input.GetKey("space"))
+      {
+        if (reset == true) {
+             SceneManager.LoadScene("flappyBird");
+             Time.timeScale = 1;
+             reset = false;
+         }
+        transform.Translate(Vector3.forward * Time.deltaTime*2);
+      }else{
+        transform.Translate(Vector3.back * Time.deltaTime*2);
+      }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+         Time.timeScale = 0;
+         reset = true;
+    }
+}
