a24fb1752ee011bb94226ebbad96b66b0b5adb47
tatyana-p <55063679+tatyana-p@users.noreply.github.com>
2019-12-15 11:34:23 -0500

    Update select_attribute.py

--- select_attribute.py
+++ select_attribute.py
@@ -2,30 +2,30 @@ maya.cmds.select('OISEAU')
 sequence = maya.cmds.ls(selection=True)
 
 if len(sequence) == 0:
-  print "<la scène est vide>\n"
+  print "<la scÃ¨ne est vide>\n"
 else:
   enumerate(sequence)
   
 def attribute_read(nodeName, attributeName):
-  """fonction de lecture d'une valeur d'un attribut assigné à un noeud"""
+  """fonction de lecture d'une valeur d'un attribut assignÃ© Ã  un noeud"""
 
-  # valider si le noeud possède l'attribut
+  # valider si le noeud possÃ¨de l'attribut
   if maya.cmds.objExists("%s.%s" % (nodeName, attributeName)):
     # extraire et retourner la valeur de l'attribut
     return maya.cmds.getAttr("%s.%s" % (nodeName, attributeName))
   else:
-    print u"<fonction 'attribute_read' annulée, car l'attribut '%s' n'existe pas sur le noeud '%s'>" % (attributeName, nodeName)
+    print u"<fonction 'attribute_read' annulÃ©e, car l'attribut '%s' n'existe pas sur le noeud '%s'>" % (attributeName, nodeName)
     return None
     
 def attribute_write(nodeName, attributeName, attributeValue):
-  """fonction d'écriture d'une valeur dans un attribut assigné à un noeud"""
+  """fonction d'Ã©criture d'une valeur dans un attribut assignÃ© Ã  un noeud"""
 
-  # valider si le noeud possède l'attribut
+  # valider si le noeud possÃ¨de l'attribut
   if maya.cmds.objExists("%s.%s" % (nodeName, attributeName)):
     # assigner la nouvelle valeur de l'attribut
     maya.cmds.setAttr("%s.%s" % (nodeName, attributeName), attributeValue)
   else:
-    print u"<fonction 'attribute_write' annulée, car l'attribut '%s' n'existe pas sur le noeud '%s'>" % (attributeName, nodeName)
+    print u"<fonction 'attribute_write' annulÃ©e, car l'attribut '%s' n'existe pas sur le noeud '%s'>" % (attributeName, nodeName)
 
 transformNodeCube = sequence[0]
 
@@ -33,15 +33,15 @@ currentTranslateX = attribute_read(transformNodeCube, 'translateX')
 currentTranslateY = attribute_read(transformNodeCube, 'translateY')
 currentTranslateZ = attribute_read(transformNodeCube, 'translateZ')
 
-print "<avant écriture la valeur de translateX est %s>" % str(currentTranslateZ)
-print "<avant écriture la valeur de translateY est %s>" % str(currentTranslateZ)
-print "<avant écriture la valeur de translateZ est %s>" % str(currentTranslateZ)
+print "<avant Ã©criture la valeur de translateX est %s>" % str(currentTranslateX)
+print "<avant Ã©criture la valeur de translateY est %s>" % str(currentTranslateY)
+print "<avant Ã©criture la valeur de translateZ est %s>" % str(currentTranslateZ)
 
 attribute_write(transformNodeCube, 'translateX', 10.0)
 attribute_write(transformNodeCube, 'translateY', 10.0)
 attribute_write(transformNodeCube, 'translateZ', 10.0)
 
 
-print "<après écriture la valeur de translateX est %s>" % str(attribute_read(transformNodeCube, 'translateX'))
-print "<après écriture la valeur de translateY est %s>" % str(attribute_read(transformNodeCube, 'translateY'))
-print "<après écriture la valeur de translateZ est %s>" % str(attribute_read(transformNodeCube, 'translateZ'))
+print "<aprÃ¨s Ã©criture la valeur de translateX est %s>" % str(attribute_read(transformNodeCube, 'translateX'))
+print "<aprÃ¨s Ã©criture la valeur de translateY est %s>" % str(attribute_read(transformNodeCube, 'translateY'))
+print "<aprÃ¨s Ã©criture la valeur de translateZ est %s>" % str(attribute_read(transformNodeCube, 'translateZ'))
